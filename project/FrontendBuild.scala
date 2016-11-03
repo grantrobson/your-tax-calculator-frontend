990a8a4cee25266512e50208ed51bb8b0a9c1455
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2016-11-03 13:56:14 +0000

    NGC-864 Play 2.5 upgrade

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,34 +1,32 @@
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object FrontendBuild extends Build with MicroService {
 
   val appName = "your-tax-calculator-frontend"
 
   override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+
 }
 
 private object AppDependencies {
-  import play.PlayImport._
+//  import play.PlayImport._
   import play.core.PlayVersion
 
-  private val microserviceBootstrapVersion = "4.2.1"
-  private val playHealthVersion = "1.1.0"
-  private val playJsonLoggerVersion = "2.1.1"      
-  private val frontendBootstrapVersion = "6.7.0"
-  private val govukTemplateVersion = "4.0.0"
-  private val playUiVersion = "4.17.0"
-  private val playPartialsVersion = "4.6.0"
+  private val microserviceBootstrapVersion = "5.4.0"
+  private val playHealthVersion = "2.0.0"
+  private val playJsonLoggerVersion = "3.0.0"
+  private val frontendBootstrapVersion = "7.5.0"
+  private val govukTemplateVersion = "5.0.0"
+  private val playUiVersion = "5.2.0"
+  private val playPartialsVersion = "5.2.0"
   private val playAuthorisedFrontendVersion = "5.8.0"
-  private val playConfigVersion = "2.1.0"
-  private val hmrcTestVersion = "1.9.0"
+  private val playConfigVersion = "3.0.0"
+  private val hmrcTestVersion = "2.0.0"
   private val scalaTestVersion = "2.2.6"
   private val pegdownVersion = "1.6.0"
-  
+
   val compile = Seq(
-    ws,
+//    ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
     "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
     "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
@@ -52,7 +50,8 @@ private object AppDependencies {
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "org.jsoup" % "jsoup" % "1.8.1" % scope,
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope
       )
     }.test
   }

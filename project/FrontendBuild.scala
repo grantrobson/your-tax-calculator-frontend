908b9bd8e0c38088139b653288df8114b5ba6ee9
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2016-11-23 13:42:33 +0000

    NGC-1041 updated logging level and dependency versions

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -14,13 +14,13 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   private val playHealthVersion = "2.0.0"
-  private val playJsonLoggerVersion = "3.0.0"
-  private val frontendBootstrapVersion = "7.9.0"
+  private val logbackJsonLoggerVersion = "3.1.0"
+  private val frontendBootstrapVersion = "7.10.0"
   private val govukTemplateVersion = "5.0.0"
   private val playUiVersion = "5.2.0"
-  private val playAuthorisedFrontendVersion = "5.8.0"
+  private val playAuthorisedFrontendVersion = "6.2.0"
   private val playConfigVersion = "3.0.0"
-  private val hmrcTestVersion = "2.0.0"
+  private val hmrcTestVersion = "2.2.0"
   private val scalaTestVersion = "2.2.6"
   private val pegdownVersion = "1.6.0"
 
@@ -29,7 +29,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
     "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
     "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-ui" % playUiVersion
